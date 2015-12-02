c7230d2be4fd12dbcb8a8bac19e2d0b84abe8504
Martin Kramer <martin.kramer@bertelsmann.de>
2015-12-02 17:22:22 +0100

    First working version of extended templatehints

--- Model/TemplateEngine/Decorator/DebugHints.php
+++ Model/TemplateEngine/Decorator/DebugHints.php
@@ -7,10 +7,26 @@
  */
 
 // @codingStandardsIgnoreFile
-namespace Shockwavemk\Smtp\Model\TemplateEngine\Decorator;
+namespace Shockwavemk\Templatehints\Model\TemplateEngine\Decorator;
+
+use Magento\Theme\Block\Adminhtml\Wysiwyg\Files\Content;
 
 class DebugHints extends \Magento\Developer\Model\TemplateEngine\Decorator\DebugHints implements \Magento\Framework\View\TemplateEngineInterface
 {
+    /**
+     * Cache
+     *
+     * @var \Magento\Framework\App\CacheInterface
+     */
+    private $_cache;
+
+    /**
+     * Cache State
+     *
+     * @var \Magento\Framework\App\Cache\StateInterface
+     */
+    protected $_cacheState;
+
     /**
      * @var \Magento\Framework\View\TemplateEngineInterface
      */
@@ -25,10 +41,17 @@ class DebugHints extends \Magento\Developer\Model\TemplateEngine\Decorator\Debug
      * @param \Magento\Framework\View\TemplateEngineInterface $subject
      * @param bool $showBlockHints Whether to include block into the debugging information or not
      */
-    public function __construct(\Magento\Framework\View\TemplateEngineInterface $subject, $showBlockHints)
+    public function __construct(
+        \Magento\Framework\View\TemplateEngineInterface $subject,
+        $showBlockHints,
+        \Magento\Framework\View\Element\Context $context
+    )
     {
         $this->_subject = $subject;
         $this->_showBlockHints = $showBlockHints;
+
+        $this->_cache = $context->getCache();
+        $this->_cacheState = $context->getCacheState();
     }
 
     /**
@@ -39,43 +62,203 @@ class DebugHints extends \Magento\Developer\Model\TemplateEngine\Decorator\Debug
     public function render(\Magento\Framework\View\Element\BlockInterface $block, $templateFile, array $dictionary = [])
     {
         $result = $this->_subject->render($block, $templateFile, $dictionary);
-        if ($this->_showBlockHints) {
-            $result = $this->_renderBlockHints($result, $block);
-        }
-        $result = $this->_renderTemplateHints($result, $templateFile);
+        $result = $this->_renderAdvancedTemplateHints($result, $templateFile, $block);
         return $result;
     }
 
     /**
      * Insert template debugging hints into the rendered block contents
      *
+     * @param $block \Magento\Framework\View\Element\AbstractBlock|\Magento\Framework\View\Element\BlockInterface
      * @param string $blockHtml
      * @param string $templateFile
      * @return string
      */
-    protected function _renderTemplateHints($blockHtml, $templateFile)
+    protected function _renderAdvancedTemplateHints($blockHtml, $templateFile, $block)
     {
+        $moduleName = var_export($block->getModuleName(), true);
+
+        $cacheTags = implode(',<br/>', $this->getCacheTags($block));
+        $cacheKeyInfoString = implode(',<br/>', $block->getCacheKeyInfo());
+
+        $parentString = $this->getParentString($block);
+        $lifetimeString = !empty($this->getCacheLifetime($block)) ? $this->getCacheLifetime($block) : '-';
+        $colors = $this->getCacheColors($block);
+
+        $content = <<<Content
+        <table>
+            <tr><th>General</th></tr>
+            <tr><td><strong>Template:</strong></td><td>{$templateFile}</td></tr>
+            <tr><td><strong>Type:</strong></td><td>{$block->getType()}</td></tr>
+            <tr><td><strong>Layout:</strong></td><td>{$block->getNameInLayout()}</td></tr>
+            <tr><td><strong>Module:</strong></td><td>{$moduleName}</td></tr>
+            <tr><td><strong>Parent:</strong></td><td>{$parentString}</td></tr>
+            <tr><td>&nbsp;</td></tr>
+            <tr><th>Cache</th></tr>
+            <tr><td><strong>Key:</strong></td><td>{$block->getCacheKey()}</td><tr>
+            <tr><td><strong>Tags:</strong></td><td>{$cacheTags}</td><tr>
+            <tr><td><strong>Lifetime:</strong></td><td>{$lifetimeString}</td><tr>
+            <tr><td><strong>Info:</strong></td><td>{$cacheKeyInfoString}</td><tr>
+            <tr><td><strong>Status:</strong></td><td>{$this->getCacheStatusString($block)}</td><tr>
+        </table>
+Content;
+
         return <<<HTML
-<div class="debugging-hints" style="position: relative; border: 1px dotted red; margin: 6px 2px; padding: 18px 2px 2px 2px;">
-<div class="debugging-hint-template-file" style="position: absolute; top: 0; padding: 2px 5px; font: normal 11px Arial; background: red; left: 0; color: white; white-space: nowrap;" onmouseover="this.style.zIndex = 999;" onmouseout="this.style.zIndex = 'auto';" title="{$templateFile}">{$templateFile}</div>
+<div class="debugging-hints" data-ot="{$content}" data-ot-target="true"
+     data-ot-target-joint="top left" data-ot-background="{$colors['light']}" data-ot-border-color="{$colors['dark']}"
+     style="border: 1px dotted {$colors['dark']}; ">
 {$blockHtml}
 </div>
 HTML;
     }
 
     /**
-     * Insert block debugging hints into the rendered block contents
+     * Get tags array for saving cache
+     * @see AbstractBlock, method in class is protected in original version
      *
-     * @param string $blockHtml
-     * @param \Magento\Framework\View\Element\BlockInterface $block
+     * @param $block \Magento\Framework\View\Element\AbstractBlock
+     * @return array
+     */
+    protected function getCacheTags($block)
+    {
+        if (!$block->hasData('cache_tags')) {
+            $tags = [];
+        } else {
+            $tags = $block->getData('cache_tags');
+        }
+        $tags[] = $block::CACHE_GROUP;
+        return $tags;
+    }
+
+    /**
+     * Get block cache life time
+     * @see AbstractBlock, method in class is protected in original version
+     *
+     * @return int
+     */
+    protected function getCacheLifetime($block)
+    {
+        if (!$block->hasData('cache_lifetime')) {
+            return null;
+        }
+        return $block->getData('cache_lifetime');
+    }
+
+    /**
+     * Returns cache status for given block.
+     * @see AbstractBlock, method in class is protected in original version
+     *
+     * @param $block
+     * @return bool
+     */
+    protected function getCacheStatus($block)
+    {
+        if ($this->getCacheLifetime($block) === null || !$this->_cacheState->isEnabled($block::CACHE_GROUP)) {
+            return false;
+        }
+
+        $cacheKey = $block->getCacheKey();
+
+        if(empty($cacheKey))
+        {
+            return false;
+        }
+
+        $cacheData = $this->_cache->load($cacheKey);
+
+        return isset($cacheData);
+    }
+
+    /**
+     * @param $block
+     * @return bool
+     */
+    protected function getCacheState($block)
+    {
+        return $this->_cacheState->isEnabled($block::CACHE_GROUP);
+    }
+
+    /**
+     * Returns array for colorizing tool-tips depending on cache state / status
+     * Todo: Refactor this
+     *
+     * @param $block
+     * @return array
+     */
+    protected function getCacheColors($block)
+    {
+        $cacheState = $this->getCacheState($block);
+        $cacheStatus = $this->getCacheStatus($block);
+
+        $color = array(
+            'light' => '#F6CECE',
+            'dark' => 'red'
+        );
+
+        if(!$cacheState)
+        {
+            $color = array(
+                'light' => '#F2F5A9',
+                'dark' => 'yellow'
+            );
+        }
+
+        if($cacheStatus)
+        {
+            $color = array(
+                'light' => '#D0F5A9',
+                'dark' => 'green'
+            );
+        }
+
+        return $color;
+    }
+
+    /**
+     * Returns descriptive string with detailed information about cache state / cache status
+     *
+     * @param $block
      * @return string
      */
-    protected function _renderBlockHints($blockHtml, \Magento\Framework\View\Element\BlockInterface $block)
+    protected function getCacheStatusString($block)
     {
-        $blockClass = get_class($block);
-        return <<<HTML
-<div class="debugging-hint-block-class" style="position: absolute; top: 0; padding: 2px 5px; font: normal 11px Arial; background: red; right: 0; color: blue; white-space: nowrap;" onmouseover="this.style.zIndex = 999;" onmouseout="this.style.zIndex = 'auto';" title="{$blockClass}">{$blockClass}</div>
-{$blockHtml}
-HTML;
+        $cacheState = $this->getCacheState($block);
+        $cacheStatus = $this->getCacheStatus($block);
+
+        $cacheString = '';
+
+        if(!$cacheState)
+        {
+            $cacheString .= 'Cache disabled for group:' . $block::CACHE_GROUP .'.';
+        }
+
+        if($cacheStatus)
+        {
+            $cacheString .= 'Found in cache.';
+        }
+        else
+        {
+            $cacheString .= 'Not found in cache.';
+        }
+
+        return $cacheString;
+    }
+
+    /**
+     * Returns descriptive string with name of parent block in layout
+     *
+     * @param $block
+     * @return string
+     */
+    protected function getParentString($block)
+    {
+        $parent = $block->getParentBlock();
+
+        if(!empty($parent))
+        {
+            return $parent->getNameInLayout();
+        }
+
+        return 'No parent block';
     }
 }
